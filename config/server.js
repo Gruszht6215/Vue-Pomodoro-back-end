6bb9286f57da6c49392460a94f318b625c6e091c
Gruszht6215 <kunkawee.s@ku.th>
2021-08-01 06:05:48 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6d8cad5f70e8a70429229306513910da'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '49befbef-d71b-42f9-869f-beb3235dc02b'
+};
